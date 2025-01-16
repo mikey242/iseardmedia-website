f6419cf27402aa1d844ba411cb327f370cbc9f7f
michael <michael@iseard.media>
2025-01-16 14:27:32 +0100

    feat: add gradient variations

--- src/components/Gradient.tsx
+++ src/components/Gradient.tsx
@@ -5,15 +5,12 @@ export function Gradient({
   className,
   ...props
 }: React.ComponentPropsWithoutRef<'div'>) {
-  return (
-    <div
-      {...props}
-      className={clsx(
-        className,
-        'bg-[linear-gradient(115deg,var(--tw-gradient-stops))] from-[#2BFF88] from-[10%] via-[#2BD2FF] via-[52%] to-[#FA8BFF] to-[90%] sm:bg-[linear-gradient(190deg,var(--tw-gradient-stops))]',
-      )}
-    />
-  )
+  let variant1 =
+    'bg-[linear-gradient(115deg,var(--tw-gradient-stops))] from-[#2BFF88] from-[10%] via-[#2BD2FF] via-[52%] to-[#FA8BFF] to-[90%] sm:bg-[linear-gradient(190deg,var(--tw-gradient-stops))]'
+  let variant2 =
+    'bg-[linear-gradient(115deg,var(--tw-gradient-stops))] from-[#FA8BFF] from-[0%] via-[#FF8E25] via-[50%] to-[#EFFFC8] to-[100%] sm:bg-[linear-gradient(45deg,var(--tw-gradient-stops))]'
+
+  return <div {...props} className={clsx(className, variant1)} />
 }
 
 export function GradientBackground() {
