ae230807354bc7e519705c812c78ccd04edaaad6
michael <michael@iseard.media>
2025-01-20 16:05:20 +0100

    fix: add latin subset to Poppins

--- src/components/BaseLayout.tsx
+++ src/components/BaseLayout.tsx
@@ -26,6 +26,7 @@ export async function generateMetadata(): Promise<Metadata> {
 }
 
 const poppins = Poppins({
+  subsets: ['latin'],
   weight: ['400', '500', '700'],
   variable: '--font-poppins',
 })
