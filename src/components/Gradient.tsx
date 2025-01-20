1d6ecada59aba11c05fa1293a7349dd86dd4e882
michael <michael@iseard.media>
2025-01-20 15:29:33 +0100

    feat: add grid pattern to gradient

--- src/components/Gradient.tsx
+++ src/components/Gradient.tsx
@@ -1,16 +1,36 @@
 import { clsx } from 'clsx'
 import React from 'react'
+import { GridPattern } from '@/components/GridPattern'
+
+let variant1 =
+  'bg-[linear-gradient(115deg,var(--tw-gradient-stops))] from-[#2BFF88] from-[10%] via-[#2BD2FF] via-[52%] to-[#FA8BFF] to-[90%] sm:bg-[linear-gradient(190deg,var(--tw-gradient-stops))]'
+let variant2 =
+  'bg-[linear-gradient(115deg,var(--tw-gradient-stops))] from-[#FA8BFF] from-[0%] via-[#FF8E25] via-[50%] to-[#EFFFC8] to-[100%] sm:bg-[linear-gradient(45deg,var(--tw-gradient-stops))]'
+let variant3 =
+  'bg-[linear-gradient(115deg,var(--tw-gradient-stops))] from-[#fff1be] from-[28%] via-[#ee87cb] via-[70%] to-[#b060ff]'
+
+let active = variant2
 
 export function Gradient({
   className,
+  showGrid = false,
   ...props
-}: React.ComponentPropsWithoutRef<'div'>) {
-  let variant1 =
-    'bg-[linear-gradient(115deg,var(--tw-gradient-stops))] from-[#2BFF88] from-[10%] via-[#2BD2FF] via-[52%] to-[#FA8BFF] to-[90%] sm:bg-[linear-gradient(190deg,var(--tw-gradient-stops))]'
-  let variant2 =
-    'bg-[linear-gradient(115deg,var(--tw-gradient-stops))] from-[#FA8BFF] from-[0%] via-[#FF8E25] via-[50%] to-[#EFFFC8] to-[100%] sm:bg-[linear-gradient(45deg,var(--tw-gradient-stops))]'
-
-  return <div {...props} className={clsx(className, variant2)} />
+}: React.ComponentPropsWithoutRef<'div'> & {
+  className: string
+  showGrid?: boolean // Optional, since you provide a default value
+}) {
+  return (
+    <div className="-z-10">
+      <div {...props} className={clsx(className, active)} />
+      {showGrid && (
+        <GridPattern
+          className="absolute inset-x-0 -top-14 h-[1000px] w-full fill-neutral-50 stroke-neutral-950/5 opacity-10 [mask-image:linear-gradient(to_bottom_left,white_40%,transparent_50%)]"
+          yOffset={-96}
+          interactive
+        />
+      )}
+    </div>
+  )
 }
 
 export function GradientBackground() {
@@ -18,8 +38,8 @@ export function GradientBackground() {
     <div className="relative mx-auto max-w-7xl">
       <div
         className={clsx(
-          'absolute -right-60 -top-44 h-60 w-[36rem] transform-gpu md:right-0',
-          'bg-[linear-gradient(115deg,var(--tw-gradient-stops))] from-[#fff1be] from-[28%] via-[#ee87cb] via-[70%] to-[#b060ff]',
+          'absolute -right-60 -top-44 h-60 w-[36rem] transform-gpu',
+          active,
           'rotate-[-10deg] rounded-full blur-3xl',
         )}
       />
--- src/components/GridPattern.tsx
+++ src/components/GridPattern.tsx
@@ -1,9 +1,9 @@
 'use client'
 
-import React, { useEffect, useId, useRef, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { motion } from 'framer-motion'
 
-function Block({
+function Circle({
   x,
   y,
   ...props
@@ -12,9 +12,11 @@ function Block({
   y: number
 }) {
   return (
-    <motion.path
-      transform={`translate(${-32 * y + 96 * x} ${160 * y})`}
-      d="M45.119 4.5a11.5 11.5 0 0 0-11.277 9.245l-25.6 128C6.82 148.861 12.262 155.5 19.52 155.5h63.366a11.5 11.5 0 0 0 11.277-9.245l25.6-128c1.423-7.116-4.02-13.755-11.277-13.755H45.119Z"
+    <motion.circle
+      transform={`translate(${-33 * y + 156 * x} ${156 * y})`}
+      cx="75"
+      cy="75"
+      r="75"
       {...props}
     />
   )
@@ -28,8 +30,7 @@ export function GridPattern({
   yOffset?: number
   interactive?: boolean
 }) {
-  let id = useId()
-  let ref = useRef<React.ElementRef<'svg'>>(null)
+  let ref = useRef<React.ComponentRef<'svg'>>(null)
   let currentBlock = useRef<[x: number, y: number]>(undefined)
   let counter = useRef(0)
   let [hoveredBlocks, setHoveredBlocks] = useState<
@@ -61,11 +62,11 @@ export function GridPattern({
         return
       }
 
-      x = x - rect.width / 2 - 32
+      x = x - rect.width / 2 - 33
       y = y - yOffset
-      x += Math.tan(32 / 160) * y
-      x = Math.floor(x / 96)
-      y = Math.floor(y / 160)
+      x += Math.tan(33 / 156) * y
+      x = Math.floor(x / 156)
+      y = Math.floor(y / 156)
 
       if (currentBlock.current?.[0] === x && currentBlock.current?.[1] === y) {
         return
@@ -91,39 +92,25 @@ export function GridPattern({
 
   return (
     <svg ref={ref} aria-hidden="true" {...props}>
-      <rect width="100%" height="100%" fill={`url(#${id})`} strokeWidth="0" />
       <svg x="50%" y={yOffset} strokeWidth="0" className="overflow-visible">
         {staticBlocks.map((block) => (
-          <Block key={`${block}`} x={block[0]} y={block[1]} />
+          <Circle key={`${block}`} x={block[0]} y={block[1]} />
         ))}
         {hoveredBlocks.map((block) => (
-          <Block
+          <Circle
             key={block[2]}
             x={block[0]}
             y={block[1]}
             animate={{ opacity: [0, 1, 0] }}
             transition={{ duration: 1, times: [0, 0, 1] }}
-            onAnimationComplete={() => {
-              setHoveredBlocks((blocks) =>
-                blocks.filter((b) => b[2] !== block[2]),
-              )
-            }}
+            // onAnimationComplete={() => {
+            //   setHoveredBlocks((blocks) =>
+            //     blocks.filter((b) => b[2] !== block[2]),
+            //   )
+            // }}
           />
         ))}
       </svg>
-      <defs>
-        <pattern
-          id={id}
-          width="96"
-          height="480"
-          x="50%"
-          patternUnits="userSpaceOnUse"
-          patternTransform={`translate(0 ${yOffset})`}
-          fill="none"
-        >
-          <path d="M128 0 98.572 147.138A16 16 0 0 1 82.883 160H13.117a16 16 0 0 0-15.69 12.862l-26.855 134.276A16 16 0 0 1-45.117 320H-116M64-160 34.572-12.862A16 16 0 0 1 18.883 0h-69.766a16 16 0 0 0-15.69 12.862l-26.855 134.276A16 16 0 0 1-109.117 160H-180M192 160l-29.428 147.138A15.999 15.999 0 0 1 146.883 320H77.117a16 16 0 0 0-15.69 12.862L34.573 467.138A16 16 0 0 1 18.883 480H-52M-136 480h58.883a16 16 0 0 0 15.69-12.862l26.855-134.276A16 16 0 0 1-18.883 320h69.766a16 16 0 0 0 15.69-12.862l26.855-134.276A16 16 0 0 1 109.117 160H192M-72 640h58.883a16 16 0 0 0 15.69-12.862l26.855-134.276A16 16 0 0 1 45.117 480h69.766a15.999 15.999 0 0 0 15.689-12.862l26.856-134.276A15.999 15.999 0 0 1 173.117 320H256M-200 320h58.883a15.999 15.999 0 0 0 15.689-12.862l26.856-134.276A16 16 0 0 1-82.883 160h69.766a16 16 0 0 0 15.69-12.862L29.427 12.862A16 16 0 0 1 45.117 0H128" />
-        </pattern>
-      </defs>
     </svg>
   )
 }
