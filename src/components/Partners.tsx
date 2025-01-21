2d0a41f83a3a3c652b4a498fe5ed296a83839819
michael <michael@iseard.media>
2025-01-21 15:41:25 +0100

    chore: update partner logos

--- src/components/Partners.tsx
+++ src/components/Partners.tsx
@@ -1,5 +1,10 @@
 import Image from 'next/image'
 import logoPearson from '@/images/logos/pearson.svg'
+import logoMuzee from '@/images/logos/muzee.svg'
+import logoDeRegentes from '@/images/logos/deregentes.svg'
+import logoEarthCalling from '@/images/logos/earthcalling.svg'
+import logoBDH from '@/images/logos/bdh.png'
+import logoTrias from '@/images/logos/trias.png'
 import { useTranslations } from 'next-intl'
 
 export function Partners() {
@@ -10,41 +15,48 @@ export function Partners() {
         <h2 className="text-center text-lg/8 font-bold text-gray-900">
           {t('heading')}
         </h2>
-        <div className="mx-auto mt-10 grid max-w-lg grid-cols-4 items-center gap-x-8 gap-y-10 sm:max-w-xl sm:grid-cols-6 sm:gap-x-10 lg:mx-0 lg:max-w-none lg:grid-cols-5">
+        <div className="mx-auto mt-10 grid max-w-lg grid-cols-4 items-center gap-x-8 gap-y-10 text-black sm:max-w-xl sm:grid-cols-6 sm:gap-x-10 lg:mx-0 lg:max-w-none lg:grid-cols-6">
           <Image
-            alt="Transistor"
+            alt="Pearson"
             src={logoPearson}
             width={158}
             height={48}
             className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
           />
-          <img
-            alt="Reform"
-            src="https://tailwindui.com/plus/img/logos/158x48/reform-logo-gray-900.svg"
+          <Image
+            alt="Muzee"
+            src={logoMuzee}
             width={158}
             height={48}
             className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
           />
-          <img
-            alt="Tuple"
-            src="https://tailwindui.com/plus/img/logos/158x48/tuple-logo-gray-900.svg"
+          <Image
+            alt="Earth Calling"
+            src={logoEarthCalling}
             width={158}
             height={48}
             className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
           />
-          <img
-            alt="SavvyCal"
-            src="https://tailwindui.com/plus/img/logos/158x48/savvycal-logo-gray-900.svg"
+          <Image
+            alt="DeRegentes"
+            src={logoDeRegentes}
             width={158}
             height={48}
             className="col-span-2 max-h-12 w-full object-contain sm:col-start-2 lg:col-span-1"
           />
-          <img
-            alt="Statamic"
-            src="https://tailwindui.com/plus/img/logos/158x48/statamic-logo-gray-900.svg"
+          <Image
+            alt="Bibliotheek Den Haag"
+            src={logoBDH}
             width={158}
             height={48}
-            className="col-span-2 col-start-2 max-h-12 w-full object-contain sm:col-start-auto lg:col-span-1"
+            className="col-span-2 max-h-12 w-full object-contain sm:col-start-auto lg:col-span-1"
+          />
+          <Image
+            alt="Stichting Trias"
+            src={logoTrias}
+            width={158}
+            height={48}
+            className="col-span-2 max-h-12 w-full object-contain sm:col-start-auto lg:col-span-1"
           />
         </div>
       </div>
